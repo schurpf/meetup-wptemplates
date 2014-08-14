0f6ad3132b143d423e9fcc2906913e61cc8ca0b2
Schurpf Mac <michael@schurpf.com>
2014-08-13 21:42:51 -0500

    single.php

---
+++ single.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<p>WordPress rocks!</p>
+<?php if ( have_posts() ) : ?>
+			<div class="wp-loop single">
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+				<article>
+					<header>
+						<h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
+					</header>
+					<p><?php the_author( );?><span class="article-date"><?php the_date( );?> </span></p>
+					<div class="content-wrap"><?php the_content();?></div>
+				</article>
+				<?php endwhile; ?>
+			</div><!--wp-loop-->
+<?php else : ?>
+<p>Sorry no content here</p>
+<?php endif; ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
