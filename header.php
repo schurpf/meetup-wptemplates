851228aa1e45a58ac8fe7fdceb87e71e065ce0e0
Schurpf Mac <michael@schurpf.com>
2014-08-13 19:31:44 -0500

    correct html

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The header for our theme.
+ */
+?><!DOCTYPE html>
+<html>
+<head>
+
+<?php wp_head(); ?>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php while ( have_posts() ) : the_post(); ?>
-<?php the_title();?>
+<?php get_header(); ?>
+<?php if ( have_posts() ) : ?>
 
-<?php endwhile; ?>
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			<?php the_title();?>
+
+			<?php endwhile; ?>
+
+<?php else : ?>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
